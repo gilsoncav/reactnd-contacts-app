fe9650f43bc06fc136e86ee08dd37bfdcc993d5b
Gilson Cavalcanti <gilsoncav@gmail.com>
2020-01-20 20:17:40 -0300

    deletion of contacts working

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,20 +1,21 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class ListContacts extends Component {
-    render() {
-        return <ol className='contact-list'>{
-            this.props.contacts.map(contact => (
+function ListContacts(props) {
+    return (
+        <ol className='contact-list'>{
+            props.contacts.map(contact => (
                 <li key={contact.id} className='contact-list-item'>
                     <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
                     <div className='contact-details'>
-                      <p>{contact.name}</p>
-                      <p>{contact.handle}</p>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
                     </div>
-                    <button className='contact-remove'>Remove</button>
+                    <button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>Remove</button>
                 </li>
             ))
         }</ol>
-    }
+    )
 }
+
 
 export default ListContacts
